81da2fec164f95105c2c2a6084dd255f51653ba8
Kurt Sewell <57918390+Kurt-Sewell@users.noreply.github.com>
2025-02-24 13:54:07 -0600

    minor change

--- Lab 2/hdl/riscv_single.sv
+++ Lab 2/hdl/riscv_single.sv
@@ -170,12 +170,12 @@ module maindec (input  logic [6:0] op,
        7'b0100011: controls = 12'b0_001_1_1_00_0_00_0; // sw
        7'b0110011: controls = 12'b1_xxx_0_0_00_0_10_0; // R–type
        7'b1100011: case(funct3)
-        001: controls = 12'b0_010_0_0_00_1_01_0; // bne <--- controls is wrong
+        001: controls = 12'b0_010_0_0_xx_1_01_0; // bne <--- controls is wrong
         100: controls = 12'b0_010_0_0_00_1_01_0; // blt <--- controls is wrong
         101: controls = 12'b0_010_0_0_00_1_01_0; // bge <--- controls is wrong
         110: controls = 12'b0_010_0_0_00_1_01_0; // bltu <--- controls is wrong
         111: controls = 12'b0_010_0_0_00_1_01_0; // bgeu <--- controls is wrong
-        default: controls = 12'b0_010_0_0_00_1_01_0; // beq
+        default: controls = 12'b0_010_0_0_xx_1_01_0; // beq
         endcase
        7'b0010011: controls = 12'b1_000_1_0_00_0_10_0; // I–type ALU
        7'b1101111: controls = 12'b1_011_0_0_10_0_00_1; // jal
